7ba2807ab737bdae803ab6861eb2b5ceb99bb581
Jake Lundberg <grove.mountain.github@nym.hush.com>
2018-11-13 10:33:49 -0800

    qa-gitflow: updating descriptiong

--- main.tf
+++ main.tf
@@ -16,7 +16,7 @@ resource "aws_instance" "ubuntu" {
     Name        = "${var.name}"
     TTL         = "${var.ttl}"
     Owner       = "${var.owner}"
-    Description = "This is a Customer2 demo description"
+    Description = "This is a Customer demo description"
   }
 }
 
